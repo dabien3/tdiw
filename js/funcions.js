8d79ab7a77925ca9762bb37f7c97e78f2c57563c
dabien3 <davada2509@gmail.com>
2025-10-08 17:11:34 +0200

    UAB css

--- js/funcions.js
+++ js/funcions.js
@@ -1,3 +1,7 @@
 function confirmaRegistre(){
     //completa
+    alert("Registran't usuari")
+    console.log("Registran't usuari")
+    let formDiv = document.getElementById("formDiv")
+    formDiv.innerHTML = "<p class='important'>Usuari registrat amb èxit!</p>"
 }
